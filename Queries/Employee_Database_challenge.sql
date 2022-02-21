5d82c7084469e1f771115306b607967908fe0a31
Chinky Gupta <95595378+chinky0910@users.noreply.github.com>
2022-02-21 01:32:24 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -13,11 +13,15 @@ WHERE (e.birth_date BETWEEN '1952-01-01'AND'1955-12-31')
 ORDER BY e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no)emp_no,first_name,last_name,title
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title,
+to_date
 INTO unique_titles
 FROM retirement_titles
 WHERE to_date = '9999-01-01'
-ORDER BY emp_no,to_date DESC;
+ORDER BY emp_no ASC, to_date DESC;
 
 -- Retrieve the number of employees by their most recent job title who are about to retire.
 SELECT COUNT(emp_no),title
@@ -27,18 +31,19 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 -- Write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
-SELECT DISTINCT ON(e.emp_no) e.emp_no, 
-    e.first_name, 
-    e.last_name, 
-    e.birth_date,
-    de.from_date,
-    de.to_date,
-    t.title
-INTO mentorship_eligibilty
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
 FROM employees as e
-Left outer Join dept_emp as de
+INNER JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
-Left outer Join titles as t
-ON (e.emp_no = t.emp_no)
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
